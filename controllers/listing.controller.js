2d95d9dcf9c3474c873878563721e7334acf7364
Hesham243 <heshamahmed243@gmail.com>
2025-07-17 11:10:50 +0300

    Finish delete + Update routes

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -1,16 +1,63 @@
 const express = require('express')
 const router = express.Router()
 const Listing = require ('../models/listing')
+const session = require('express-session')
+const isSignedIn = require('../middleware/is-signed-in')
+
 
 // View new listing form
 router.get('/new',(req, res) => {
-  res.render('listing/new.ejs')
+  res.render('listings/new.ejs')
 })
 
-router.post('/', async (req, res) => {
-  const addedListing = await Listing.create(req.body)
-  console.log(addedListing)
-  res.send('you submitted')
+router.post('/', isSignedIn, async (req, res) => {
+  try {
+    req.body.seller = req.session.user._id
+    const addedListing = await Listing.create(req.body)
+    res.redirect('/listings/')
+  } catch (err){
+    console.log(error)
+    res.send('Something went wrong')
+  }
+})
+
+router.get('/', async (req, res) => {
+  const foundListings = await Listing.find()
+  res.render('Listings/index.ejs', {foundListings: foundListings})
+})
+
+router.get('/:listingId', async (req, res) => {
+  const foundListings = await Listing.findById(req.params.listingId).populate("seller")
+  res.render('listings/show.ejs', {foundListing: foundListings})
+})
+
+
+router.delete('/:listingId', async (req, res) => {
+  const  foundListing = await Listing.findById(req.params.listingId).populate("seller")
+
+  if (foundListing.seller._id.equals(req.session.user._id)) {
+    await foundListing.deleteOne()
+    return res.redirect('/listings')
+  }
+
+  return res.send('Not authorized')
+})
+
+
+router.get('/:listingId/edit', async (req, res) => {
+
+  const  foundListing = await Listing.findById(req.params.listingId).populate("seller")
+
+  if (foundListing.seller._id.equals(req.session.user._id)) {
+    return res.render('listings/edit.ejs', { foundListing: foundListing })
+  }else {
+    return res.send('Not authorized')
+  }
+})
+
+
+router.put('/:listingId', (req, res) => {
+  res.send(`You updated listing: ${req.params.listingId}`)
 })
 
 module.exports = router
--- models/listing.js
+++ models/listing.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const listingSchema = new mongoose.Schema({
   title: String,
   description: String,
   price: Number,
-  seller: String,
   image: String,
+  seller: { 
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
 }, {
   timestamps: true
 })
